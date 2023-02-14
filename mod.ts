24a09c509fd4e1d87906eadb8e2dc7a0dd5a7d8a
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-02-14 16:24:27 +0900

    feat(mod.ts): export types

--- mod.ts
+++ mod.ts
@@ -1,15 +1,11 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-import {
-  type Middleware,
-} from "https://deno.land/x/http_middleware@1.0.0-beta.1/mod.ts";
-import {
-  type Options,
-  withCors,
-} from "https://deno.land/x/cors_protocol@1.0.0-beta.6/mod.ts";
+import { type Middleware, type Options, withCors } from "./deps.ts";
+export { type Middleware, type Options } from "./deps.ts";
 
 /** Create CORS middleware.
+ * @param options CORS header options.
  *
  * @example
  * ```ts
